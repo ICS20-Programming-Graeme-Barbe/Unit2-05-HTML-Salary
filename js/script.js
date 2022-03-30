39892839528d3fed96338afae1747ef2896cce5f
GraemeBrabe Barbe <99273811+GraemeBarbe@users.noreply.github.com>
2022-03-30 13:29:44 +0000

    index.html

--- js/script.js
+++ js/script.js
@@ -6,9 +6,11 @@
 // This function calculates the total amount of money and how much taxes you will pay
  'use strict'
 function calculate () {
-	const TAXE_RATE = (0.1805)
+	const TAX_RATE = 0.1805
 	let hours = parseFloat(document.getElementById('hours').value)
 	let rate = parseFloat(document.getElementById('rate').value)
 	let total = (hours * rate)
-	document.getElementById('total').innerHTML = 'The area is ' + total + ' cm²'
+	let tax = (total * TAX_RATE)
+	document.getElementById('total').innerHTML = 'Your salary would be ' + total.toFixed(2) + '$ without taxes' 
+	document.getElementById('tax').innerHTML = 'The government will take ' + tax.toFixed(2) + '$'
 }
